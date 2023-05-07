2a94b078319c4ed8541aafdc3850023c0e5471e7
Abdelrahman70150 <102836244+Abdelrahman70150@users.noreply.github.com>
2023-05-07 13:53:11 +0300

    add radio Screen

--- lib/screens/radio_screen/radio_screen.dart
+++ lib/screens/radio_screen/radio_screen.dart
@@ -1,9 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:islami/const/constan.dart';
 
 class RadioScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset('assets/images/radio_image.png'),
+        SizedBox(height: 74,),
+        Text('إذاعة القرآن الكريم',
+        style: GoogleFonts.elMessiri(
+          fontSize: 25
+        ),),
+        SizedBox(height: 65,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(onPressed: (){},
+                icon: Icon(
+                  Icons.skip_previous,
+                  color: mainColor,
+                ),
+              iconSize: 40,
+              alignment: AlignmentDirectional.center,
+            ),
+            IconButton(onPressed: (){},
+              icon: Icon(
+                Icons.play_arrow,
+                color: mainColor,
+              ),
+              iconSize: 55,
+              alignment: AlignmentDirectional.center,
+              padding: EdgeInsets.only(),
+            ),
+            IconButton(onPressed: (){},
+              icon: Icon(
+                Icons.skip_next,
+                color: mainColor,
+              ),
+              iconSize: 40,
+              alignment: AlignmentDirectional.center,
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
