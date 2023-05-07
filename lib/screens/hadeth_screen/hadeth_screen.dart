af19b43fa41968ba5b0d6bb6f3cefc0396f51bb7
Abdelrahman70150 <102836244+Abdelrahman70150@users.noreply.github.com>
2023-05-07 21:35:53 +0300

    add HadethScreen add hadeth_details_screen.dart add hadeth.txt

--- lib/screens/hadeth_screen/hadeth_screen.dart
+++ lib/screens/hadeth_screen/hadeth_screen.dart
@@ -1,9 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:islami/arg/hadeth_arg.dart';
+import 'package:islami/const/constan.dart';
+import 'package:islami/screens/hadeth_details_screen/hadeth_details_screen.dart';
 
-class HadethScreen extends StatelessWidget {
+
+class HadethScreen extends StatefulWidget {
+  @override
+  State<HadethScreen> createState() => _HadethScreenState();
+}
+
+class _HadethScreenState extends State<HadethScreen> {
+  List<HadethModel> allAhadeth=[];
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    if(allAhadeth.isEmpty){
+      loadFile();
+    }
+    return Column(
+      children: [
+        Image.asset('assets/images/hadeth_logo.png'),
+      Divider(
+        thickness: 1,
+        color: mainColor,
+      ),
+      Text('الأحاديث',
+      style: GoogleFonts.elMessiri(
+        fontSize: 25,
+      ),),
+        Divider(
+          thickness: 1,
+          color: mainColor,
+        ),
+        Expanded(
+          child: ListView.separated(
+            physics: BouncingScrollPhysics(),
+            itemBuilder: (context,index)=>InkWell(
+            onTap: (){
+              Navigator.pushNamed(context, HadethDetailsScreen.routName,
+                arguments: allAhadeth[index],
+
+              );
+            },
+            child: Text(
+             allAhadeth[index].hadethName,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.elMessiri(
+              fontSize: 25,
+              fontWeight: FontWeight.bold
+            ),),
+          ),
+          itemCount: allAhadeth.length,
+          separatorBuilder: (contex,index)=>SizedBox(
+            height: 10,
+          ),
+          ),
+        ),
+      ],
+    );
   }
+
+void loadFile(){
+  rootBundle.loadString('assets/files/ahadeth.txt').then((ahadethFile) {
+    //دى الأحاديث كلها
+    List<String> AhadethContent = ahadethFile.split('#');
+    // همشى على كل حديث اقطعه
+    for(int i=0 ; i<AhadethContent.length; i++){
+      int lastIndex =AhadethContent[i].trim().indexOf('\n');
+      String title =AhadethContent[i].trim().substring(0,lastIndex);
+      String content = AhadethContent[i].trim().substring(lastIndex+1);
+    // for(int i=0 ; i<AhadethContent.length; i++){
+    //   List<String> SingleAhadethLines = AhadethContent[i].trim().split('\n');
+    //   // تايتل الحديث نفسه
+    //   String title = SingleAhadethLines[0];
+    //   //شيلت منه التايتل
+    //   SingleAhadethLines.removeAt(0);
+    //   // بقيت الحديث من غير التايتل
+    //   List<String> content =SingleAhadethLines;
+      HadethModel hadethModel =    HadethModel(content: content, hadethName: title);
+      allAhadeth.add(hadethModel);
+
+    setState(() {
+
+    });
+    }
+
+  }).catchError((error){});
+}
 }
